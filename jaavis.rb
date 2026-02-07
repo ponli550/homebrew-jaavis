3f2c79acf0080ea2b7ebb7a17406731f4e2ff470
ponli550 <nazrijz336@gmail.com>
2026-02-08 02:37:42 +0800

    Update Jaavis to version 1.0.8

--- jaavis.rb
+++ jaavis.rb
@@ -1,8 +1,8 @@
 class Jaavis < Formula
   desc "One-Army AI Orchestrator & Skill Library"
   homepage "https://github.com/ponli550/JaavisCLI"
-  url "https://github.com/ponli550/JaavisCLI/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "adb79c7d55005a7831d356108cb6875ad3ae8c214b91ad28df855e7b9125bc49"
+  url "https://github.com/ponli550/JaavisCLI/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "b345a74cb2f15797848f91965a5c6e6f37eca043aeb5a3fd643979628cb79c74"
   license "MIT"
 
   depends_on "python@3.11"
